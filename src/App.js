0bf24f08e776a8ea977ebc7215f8015d1404b7ca
Rodrigo Pimpao <rodrigo.pimpao94@gmail.com>
2020-08-22 01:48:29 -0300

    Render repositories.

--- src/App.js
+++ src/App.js
@@ -25,13 +25,16 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repositories.map(repo => {
+            return <li key={repo.id}>
+              {repo.title}
+              <button onClick={() => handleRemoveRepository(repo.id)}>
+                Remover
+              </button>
+            </li>
+          })
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
